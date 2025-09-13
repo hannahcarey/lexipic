345089b2a2d166fdefc8aa6c6b32562d91103f02
Hannah <hcarey890@gmail.com>
2025-09-13 19:22:11 -0400

    question.py -> utils.py

--- backend/question/utils.py
+++ backend/question/utils.py
@@ -1,18 +1,31 @@
 from anthropic import Anthropic
 import os, sys,json
 
-# this module recieves scene data from the vision output, and returns 3 questions.
+valid_levels = {"A1", "A2", "B1", "B2", "C1", "C2"}
 
+def validate_data(img_data, user_data):
+    if (user_data.level not in valid_levels):
+        return (None, "Invalid user level")
+    if (img_data.description == ""):
+        return (None, "Bad data from vision model")
+    return true
+
+def get_key():
+    api_key = os.environ.get("ANTHROPIC_API_KEY")
+    if (api_key == None):
+        sys.exit("No value set for ANTHROPIC_API_KEY in os.environ.")
+    return api_key
+
+# recieves scene data from the vision output, and returns 3 questions.
 def get_questions(img_data, user_data):
+    out = validate_data(img_data, user_data)
+    if (out[0]==None):
+        sys.exit(out[1])
+
     # user_data should be like {"language": "English", "level": "A2"}
     # img_data should have description, some kind of id, and maybe info on bounding boxes
 
-    api_key = os.environ.get("ANTHROPIC_API_KEY")
-
-    if (api_key == None):
-        sys.exit("No value set for ANTHROPIC_API_KEY in os.environ.")
-
-    client = Anthropic(api_key = api_key)
+    client = Anthropic(api_key=get_key())
 
     scene_desc = img_data.description
     language = user_data.language
@@ -38,4 +51,5 @@ def get_questions(img_data, user_data):
 
     return json.loads(message.content[0].text)
 
-
+#def get_feedback(question, answer):
+    
